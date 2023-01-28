e7b560d46f2faa19704eef4032dea9a67d2894c2
Omaraliev Eric <theslyfox43@gmail.com>
2023-01-28 21:16:14 +0600

    added validation and finished typesetting

--- presentation/src/main/java/com/geektech/presentation/ui/fragments/sign/in/SignInFragment.kt
+++ presentation/src/main/java/com/geektech/presentation/ui/fragments/sign/in/SignInFragment.kt
@@ -1,18 +1,23 @@
-package com.geektech.presentation.fragments.sign.`in`
+package com.geektech.presentation.ui.fragments.sign.`in`
 
 import android.os.Bundle
 import android.view.View
-import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
 import by.kirich1409.viewbindingdelegate.viewBinding
+import com.geektech.presentation.R
 import com.geektech.presentation.databinding.FragmentSignInBinding
 
-class SignInFragment : Fragment() {
+class SignInFragment : Fragment(R.layout.fragment_sign_in) {
 
     private val binding by viewBinding(FragmentSignInBinding::bind)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        with(binding) {
+            btnGoogle.setOnClickListener {
+                UserNameValidation.Base(etInputName).checkValidation()
+            }
+        }
     }
 }
