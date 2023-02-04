af041fd6cac5a00ecdd0f99b72d04a91ba4c2343
Omaraliev Eric <theslyfox43@gmail.com>
2023-02-04 23:02:30 +0600

    cleaned the code

--- presentation/src/main/java/com/geektech/presentation/ui/activity/MainActivity.kt
+++ presentation/src/main/java/com/geektech/presentation/ui/activity/MainActivity.kt
@@ -3,7 +3,9 @@ package com.geektech.presentation.ui.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.geektech.presentation.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
