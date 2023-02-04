aa7021ce224bbd4948fc72b2617c7531fcdbba5a
Omaraliev Eric <theslyfox43@gmail.com>
2023-02-04 23:11:10 +0600

    added one tap sign in with google

--- presentation/src/main/java/com/geektech/presentation/ui/fragments/sign/in/SignInFragment.kt
+++ presentation/src/main/java/com/geektech/presentation/ui/fragments/sign/in/SignInFragment.kt
@@ -1,22 +1,68 @@
 package com.geektech.presentation.ui.fragments.sign.`in`
 
+import android.app.Activity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.geektech.presentation.R
 import com.geektech.presentation.databinding.FragmentSignInBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class SignInFragment : Fragment(R.layout.fragment_sign_in) {
 
+    @Inject
+    lateinit var gsc: GoogleSignInClient
     private val binding by viewBinding(FragmentSignInBinding::bind)
-
+    private val viewModel: SignInViewModel by viewModels()
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val resultLauncher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+                if (result.resultCode == Activity.RESULT_OK) {
+                    val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+                    viewModel.signInWithGoogle(task.result.idToken!!,
+                        onSuccess = {
+                            findNavController().navigate(R.id.action_signInFragment_to_homeFragment)
+                        },
+                        onError = {
+                            Toast.makeText(
+                                requireContext(),
+                                "something was wrong", Toast.LENGTH_SHORT
+                            ).show()
+                        })
+                }
+            }
+
         with(binding) {
             btnGoogle.setOnClickListener {
-                UserNameValidation.Base(etInputName).checkValidation()
+                if (etInputName.text.isNotEmpty()) {
+                    etInputName.apply {
+                        setBackgroundResource(R.drawable.normal_edit_text)
+                        setCompoundDrawablesWithIntrinsicBounds(0, 0, 0, 0)
+                    }
+                    resultLauncher.launch(gsc.signInIntent)
+                } else {
+                    etInputName.apply {
+                        setBackgroundResource(R.drawable.error_edit_text)
+                        setCompoundDrawablesWithIntrinsicBounds(
+                            0,
+                            0,
+                            R.drawable.ic_priority_high,
+                            0
+                        )
+                    }
+
+                }
             }
         }
     }
